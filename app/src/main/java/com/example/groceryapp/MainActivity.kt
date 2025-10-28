e4a63953c55b04076b724841891420d6f61502e6
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-10-28 12:43:29 -0700

    Created custom header

--- app/src/main/java/com/example/groceryapp/MainActivity.kt
+++ app/src/main/java/com/example/groceryapp/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.groceryapp.ui.theme.GroceryAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,6 +36,44 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun LazyColumnWithStickyHeader() {
+
+    // create Data source
+    val groupedItems = mapOf<String, List<String>>(
+        "fruits" to listOf("Apple", "Bananas", "Cherry", "Mango", "Grape", "Watermelon"),
+        "vegetables" to listOf("Carrot", "Lettuce", "Broccoli", "Spinach", "Tomato", "Cucumber"),
+        "dairy" to listOf("Milk", "Cheese", "Yogurt", "Butter", "Sour Cream", "Cream Cheese"),
+        "meat" to listOf("Beef", "Chicken", "Pork", "Lamb", "Turkey", "Fish"),
+        "grains" to listOf("Rice", "Pasta", "Oats", "Quinoa"),
+    )
+
+    // create lazy column
+    LazyColumn {
+        groupedItems.forEach {  (header, items) ->
+            stickyHeader {
+                CustomHeader(header)
+            }
+
+            items(items) {item ->
+                Text(text = item)
+            }
+        }
+    }
+}
+
+@Composable
+fun CustomHeader(title: String) {
+    Text(
+        title,
+        fontSize = 32.sp,
+        modifier = Modifier.fillMaxWidth()
+        .background(color = Color.Red)
+        .padding(8.dp)
+    )
 }
 
 @Composable
